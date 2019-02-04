6132f278575251fdfd92537facfe9abb7f862d5f
alberto <alberto.alvarez512@gmail.com>
2019-02-03 21:14:03 -0500

    Data loading

--- Modelo/Estudio.cs
+++ Modelo/Estudio.cs
@@ -1,15 +1,126 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Modelo
 {
+
+
     class Estudio
     {
+
+        public static int FECHA = 0;
+        public static int MUESTREO_No = 1;
+        public static int PUNTO_MUESTREO = 2;
+        public static int PUNTO = 3;
+        public static int RIO = 4;
+        public static int COORDENADAS = 5;
+        public static int ICA_IDEAM = 6;
+        public static int OD = 7;
+        public static int PH = 8;
+        public static int DQO = 9;
+        public static int CE = 10;
+        public static int SST = 11;
+
+        public static String PANCE = "Pance";
+        public static String AGUACATAL = "Aguacatal";
+        public static String CALI = "Cali";
+        public static String CAÑAVERALEJO = "Cañaveralejo";
+        public static String LILI = "lili";
+        public static String MELENDEZ = "Melendez";
+
+
+        private Hashtable hash_Muestras;
+        private String[] lineas;
+
+        public Estudio()
+        {
+            Hash_Muestras = new Hashtable();
+            inicializar_Datos();
+        }
+
+        //Este metodo se encargara de leer los datos e introducirlos en la tabla hash
+        public void inicializar_Datos() {
+
+            //Llenar hashTable de arrays
+            for (int i = 0; i<=6; i++) {
+                Hash_Muestras.Add(i, new ArrayList());
+            }
+
+            //Lee todas las lineas del archivo
+            Lineas = File.ReadAllLines("..\\..\\Datos\\Datos.csv");
+
+            //
+            foreach (var i in Lineas) {
+                String[] linea = i.Split(',');
+
+
+                if (!linea[0].Equals("Fecha"))
+                {
+
+                     String fecha = linea[FECHA];
+                     int muestraNumero = Int32.Parse(linea[MUESTREO_No]);
+                     String puntoDeMuestreo = linea[PUNTO_MUESTREO];
+                     String punto = linea[PUNTO];
+                     String rio = linea[RIO];
+                     String[] coordenadas = linea[COORDENADAS].Split(';');
+                     double ica_ideam = Double.Parse(linea[ICA_IDEAM]);
+                     double od = Double.Parse(linea[OD]);
+                     double ph = Double.Parse(linea[PH]);
+                     double dqo = Double.Parse(linea[DQO]);
+                     double ce = Double.Parse(linea[CE]);
+                     double sst = Double.Parse(linea[SST].Remove(linea[SST].Length - 1));
+
+
+                    Muestra muestra = new Muestra(fecha,muestraNumero,puntoDeMuestreo,punto,rio,coordenadas,ica_ideam,od,ph,dqo,ce,sst);
+
+                    if (muestra.Rio.Equals(PANCE))
+                    {
+                        ((ArrayList)Hash_Muestras[1]).Add(muestra);
+                    }
+
+                    if (muestra.Rio.Equals(AGUACATAL))
+                    {
+                        ((ArrayList)Hash_Muestras[2]).Add(muestra);
+                    }
+
+                    if (muestra.Rio.Equals(CALI))
+                    {
+                        ((ArrayList)Hash_Muestras[3]).Add(muestra);
+                    }
+
+                    if (muestra.Rio.Equals(CAÑAVERALEJO))
+                    {
+                        ((ArrayList)Hash_Muestras[4]).Add(muestra);
+                    }
+
+                    if (muestra.Rio.Equals(LILI))
+                    {
+                        ((ArrayList)Hash_Muestras[5]).Add(muestra);
+                    }
+
+                    if (muestra.Rio.Equals(MELENDEZ))
+                    {
+                        ((ArrayList)Hash_Muestras[6]).Add(muestra);
+                    }
+                }
+
+
+            }
+            
+            
+        }
+
+        public Hashtable Hash_Muestras { get => hash_Muestras; set => hash_Muestras = value; }
+        public string[] Lineas { get => lineas; set => lineas = value; }
+
         static void Main(string[] args)
         {
+            Estudio e = new Estudio();
         }
     }
 }
--- Modelo/Muestra.cs
+++ Modelo/Muestra.cs
@@ -8,5 +8,47 @@ namespace Modelo
 {
     class Muestra
     {
+      
+        private String fecha;
+        private int muestraNumero;
+        private String puntoDeMuestreo;
+        private String punto;
+        private String rio;
+        private String[] coordenadas;
+        private double ica_ideam;
+        private double od;
+        private double ph;
+        private double dqo;
+        private double ce;
+        private double stt;
+
+        public Muestra(String fecha,int muestraNumero, string puntoDeMuestreo, string punto, string rio, String[] coordenadas, double ica_ideam, double od, double ph, double dqo, double ce, double stt)
+        {
+            this.Fecha = fecha;
+            this.MuestraNumero = muestraNumero;
+            this.PuntoDeMuestreo = puntoDeMuestreo;
+            this.Punto = punto;
+            this.Rio = rio;
+            this.Coordenadas = coordenadas;
+            this.Ica_ideam = ica_ideam;
+            this.Od = od;
+            this.Ph = ph;
+            this.Dqo = dqo;
+            this.Ce = ce;
+            this.Stt = stt;
+        }
+
+        public String Fecha { get => fecha; set => fecha = value; }
+        public string PuntoDeMuestreo { get => puntoDeMuestreo; set => puntoDeMuestreo = value; }
+        public string Punto { get => punto; set => punto = value; }
+        public string Rio { get => rio; set => rio = value; }
+        public String[] Coordenadas { get => coordenadas; set => coordenadas = value; }
+        public double Ica_ideam { get => ica_ideam; set => ica_ideam = value; }
+        public double Od { get => od; set => od = value; }
+        public double Ph { get => ph; set => ph = value; }
+        public double Dqo { get => dqo; set => dqo = value; }
+        public double Ce { get => ce; set => ce = value; }
+        public double Stt { get => stt; set => stt = value; }
+        public int MuestraNumero { get => muestraNumero; set => muestraNumero = value; }
     }
 }
